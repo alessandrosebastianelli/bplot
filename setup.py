98f643619442cd4ebc2e96168be62b138dda3526
alessandrosebastianelli <alessandroseba1995@gmail.com>
2024-02-12 15:07:55 +0100

    updating dist

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def readme():
 # Package 
 HERE = pathlib.Path(__file__).parent
 PACKAGE_NAME  = 'bplot'
-VERSION = '0.0.9'
+VERSION = '0.0.10'
 AUTHOR = 'Alessandro Sebastianelli'
 AUTHOR_EMAIL = 'alessandro.sebastianelli1995@gmail.com'
 URL = 'https://github.com/alessandrosebastianelli/bplot.git'
