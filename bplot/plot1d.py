89c836c74c8be6fbc61bb6270c9f46949d1704a5
alessandrosebastianelli <alessandroseba1995@gmail.com>
2024-02-12 17:43:37 +0100

    adding labels and supporting more separators

--- bplot/plot1d.py
+++ bplot/plot1d.py
@@ -4,7 +4,7 @@ import pandas as pd
 import argparse
 import json
 
-def __read_data(path : str) -> pd.DataFrame:
+def __read_data(path : str, sep : str) -> pd.DataFrame:
     '''
         Load and/or process pandas dataframe using path
 
@@ -12,6 +12,7 @@ def __read_data(path : str) -> pd.DataFrame:
         ----------
         - path: string containing the path to the pandas dataframe,
                 if none a file manager will ask for the file path
+        - sep: pandas dataframe separator
 
         Returns
         -------
@@ -20,8 +21,7 @@ def __read_data(path : str) -> pd.DataFrame:
 
     if path is None: path = filedialog.askopenfilename()
 
-    db = pd.read_csv(path)
-
+    db  = pd.read_csv(path, sep=sep)
     return db
 
 def main() -> None:
@@ -38,16 +38,17 @@ def main() -> None:
     '''
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--path', default=None,   action="store", help='Path to file')
+    parser.add_argument('-p', '--path', default=None,    action="store", help='Path to file')
     parser.add_argument('-x', '--xvar', default=None,    action="store", help='Variable for x-axis')
     parser.add_argument('-y', '--yvar', default=None,    action="store", help='Variable for y-axis')
     parser.add_argument('-g', '--grid', default='False', action="store", help='Activate Grid')
     parser.add_argument('-t', '--hist', default='False', action="store", help='Plot histogram')
+    parser.add_argument('-s', '--sep',  default=',',     action="store", help='Pandas dataframe separator')
     args = parser.parse_args()
 
 
     # Read Data    
-    data = __read_data(args.path)
+    data = __read_data(args.path, args.sep)
     # Arguments  
     xvar = args.xvar
     yvar = args.yvar
@@ -59,25 +60,43 @@ def main() -> None:
         _, ax = plt.subplots(nrows=1, ncols=2, gridspec_kw={'width_ratios': [3,1]})
 
         if xvar is not None and yvar is not None: 
-            ax[0].plot(data[xvar], data[yvar])
-            ax[1].hist(data[yvar], 200, orientation='horizontal')
+            ax[0].plot(data[xvar], data[yvar], label=yvar)
+            ax[1].hist(data[yvar], 200, orientation='horizontal', label=yvar)
         elif xvar is not None and yvar is None:   
-            ax[0].plot(data[xvar], data)
-            ax[1].hist(data, 200, orientation='horizontal')
+            for col in data.columns:
+                if col is not xvar:
+                    ax[0].plot(data[xvar], data[col], label=col)
+                    ax[1].hist(data[col], 200, orientation='horizontal', label=col)
         elif xvar is None and yvar is not None:   
-            ax[0].plot(data[yvar])
-            ax[1].hist(data, 200, orientation='horizontal')
+            ax[0].plot(data[yvar], label=yvar)
+            ax[1].hist(data[yvar], 200, orientation='horizontal', label=yvar)
         else:                                     
-            ax[0].plot(data)
-            ax[1].hist(data, 200, orientation='horizontal')
+            for col in data.columns:
+                ax[0].plot(data[col], label=col)
+                ax[1].hist(data[col], 200, orientation='horizontal', label=col)
+        
+        ax[0].legend()
+        ax[1].legend()
+    
     # Plot without histogram
     else:
         _, ax = plt.subplots(nrows=1, ncols=1)
 
-        if xvar is not None and yvar is not None: ax.plot(data[xvar], data[yvar])
-        elif xvar is not None and yvar is None:   ax.plot(data[xvar], data)
-        elif xvar is None and yvar is not None:   ax.plot(data[yvar])
-        else:                                     ax.plot(data)
+        if xvar   is not None and yvar is not None: 
+            ax.plot(data[xvar], data[yvar], label=yvar)
+        elif xvar is not None and yvar is     None: 
+           for col in data.columns:
+                if col is not xvar:
+                    ax.plot(data[xvar], data[col], label=col)
+        elif xvar is     None and yvar is not None:
+            ax.plot(data[yvar], label=yvar)
+        else:                                       
+            for col in data.columns:
+                ax.plot(data[col], label=col)
+        
+        ax.legend()
+    
+    # Global settings
     if grid: ax.grid()
 
     plt.show()
